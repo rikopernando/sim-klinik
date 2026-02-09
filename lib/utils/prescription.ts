6dda75f94b5a5635d89706a6c9a0e70fec666b7b
Riko Pernando <rikopernando96@gmail.com>
2026-02-09 14:30:21 +0700

    fix: add new frequency options

--- lib/utils/prescription.ts
+++ lib/utils/prescription.ts
@@ -47,9 +47,6 @@ export const createPrescriptionItem = () => ({
  * Predefined frequency options for prescriptions
  */
 export const FREQUENCY_OPTIONS = [
-  { value: "3x1", label: "3 x 1" },
-  { value: "2x1", label: "2 x 1" },
-  { value: "1x1", label: "1 x 1" },
   { value: "1x1_pagi", label: "1 x 1 Pagi" },
   { value: "1x1_siang", label: "1 x 1 Siang" },
   { value: "1x1_malam", label: "1 x 1 Malam" },
@@ -68,5 +65,13 @@ export const FREQUENCY_OPTIONS = [
   { value: "1x1/2_pagi", label: "1 x 1/2 Pagi" },
   { value: "1x1/2_siang", label: "1 x 1/2 Siang" },
   { value: "1x1/2_malam", label: "1 x 1/2 Malam" },
+  { value: "8x1", label: "8 x 1" },
+  { value: "7x1", label: "7 x 1" },
+  { value: "6x1", label: "6 x 1" },
+  { value: "5x1", label: "5 x 1" },
+  { value: "4x1", label: "4 x 1" },
+  { value: "3x1", label: "3 x 1" },
+  { value: "2x1", label: "2 x 1" },
+  { value: "1x1", label: "1 x 1" },
   { value: "bila_perlu", label: "Bila Perlu" },
 ] as const
